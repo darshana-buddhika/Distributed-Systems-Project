58fe84f4548ccbc7527d435b457f81f9f2167dfd
darshana <darshana.soa@gmail.com>
2018-11-18 18:47:34 +0530

    added file searching basics

--- src/Main.java
+++ src/Main.java
@@ -7,32 +7,31 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String[] usernames = {"supeepa","ranula","hasini","darshana","sandaru","chamath","rangika","nadeesha","prabod","vikum"};
+		String[] usernames = { "supeepa", "ranula", "hasini", "darshana", "sandaru", "chamath", "rangika", "nadeesha",
+				"prabod", "vikum" };
 		ArrayList<UDPClient> nodesInTheNetwork = new ArrayList<>();
 
 		Scanner scanner = new Scanner(System.in);
 		try {
-//			Creating 10 nodes for the network
+			// Creating 10 nodes for the network
 			for (int i = 0; i < 10; i++) {
 
 				nodesInTheNetwork.add(new UDPClient(15000 + i * 2, usernames[i]));
 
 			}
 
-			
-			for (UDPClient node: nodesInTheNetwork) {
+			for (UDPClient node : nodesInTheNetwork) {
 				node.registerNetwork();
 			}
-			
+
 			while (true) {
 
-
 				int input = scanner.nextInt();
-				
+
 				nodesInTheNetwork.get(input).getNeighbours();
 				nodesInTheNetwork.get(input).getFiles();
-				
-			
+				nodesInTheNetwork.get(input).searchFiles("Adventures", 2);
+
 			}
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
